550323f99386f4787c8e80f96377c49a88cd7fa3
Chevez Robertson <chai@dhcp-172-30-226-122.guest.uc.edu>
2022-09-15 15:06:37 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double salesprice = 20;
+        double salestax = salesprice*0.05;
+        double salestotal = salesprice+salestax;
+        System.out.println("your total price is " + salestotal);
+    }
+}
